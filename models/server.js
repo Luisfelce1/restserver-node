2cd88a83182a7ca95c6644baabd237be7b8a8de1
Luis Felce <luisr.felce@gmail.com>
2021-08-14 22:50:47 +0200

    restserve basico listo

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+
+
+class Server {
+
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middleware (SOn funciones que van a añadirle otra funconalidad al web server )
+
+        this.middlewares();
+        //RUtas de mi aplicación
+        this.routes();
+    }
+
+    middlewares(){
+        //cors
+        this.app.use(cors());
+
+        //Lectura y Praseo del body
+        this.app.use( express.json() );
+    
+        //Directorio público
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+       this.app.use(this.usuariosPath, require('../routes/user'));
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servicio corriendo en puerto', process.env.PORT);
+        });
+    }
+}
+
+module.exports = Server;
